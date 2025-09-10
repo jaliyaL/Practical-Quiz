5ef49d0474a4f3d140c7f60ef06d8e48e54e1183
jaliyaL <jaliyalamahewa@gmail.com>
2025-09-10 11:55:55 +0530

    with concurrency

--- Set_03/p2/p2.go
+++ Set_03/p2/p2.go
@@ -5,20 +5,43 @@ import "fmt"
 /*
 Problem 2: Implement a concurrent Fibonacci generator using channels.
 */
-func fibo(n int) int {
-	if n <= 1 {
-		return n
-	}
 
-	return fibo(n-1) + fibo(n-2)
+// without channels
+// func fibo(n int) int {
+// 	if n <= 1 {
+// 		return n
+// 	}
+
+// 	return fibo(n-1) + fibo(n-2)
+// }
+
+// func RunP2() {
+// 	// normal finonachi  without channels
+// 	// var r int
+// 	for i := 0; i < 10; i++ {
+// 		r := fibo(i)
+// 		fmt.Println(r)
+// 	}
+// }
+
+// with channels
+func fibonachi(n int, ch chan int) {
+
+	x, y := 0, 1
+	for i := 0; i <= n; i++ {
+		ch <- x
+		x, y = y, x+y
+	}
+	close(ch)
+
 }
 
 func RunP2() {
-	// normal finonachi  without channels
-	// var r int
-	for i := 0; i < 10; i++ {
-		r := fibo(i)
-		fmt.Println(r)
-	}
 
+	ch := make(chan int, 10)
+	go fibonachi(10, ch)
+
+	for c := range ch {
+		fmt.Println(c)
+	}
 }
