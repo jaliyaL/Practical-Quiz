b4f399d3356567da7ed9d3723fe33ac4df59d637
jaliyaL <jaliyalamahewa@gmail.com>
2025-09-10 10:59:42 +0530

    fibo check

--- Set_03/p2/p2.go
+++ Set_03/p2/p2.go
@@ -1,11 +1,10 @@
 package p2
 
-import "fmt"
-
 /*
 Problem 2: Implement a concurrent Fibonacci generator using channels.
 */
 
 func RunP2() {
-	fmt.Println("package p2")
+	// finonachi  without channels
+
 }
