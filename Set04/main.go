aa536f28d7861b6a788837f41b0d5ecae8a1676e
jaliyaL <jaliyalamahewa@gmail.com>
2025-09-10 16:01:46 +0530

    p1 setup

--- Set04/main.go
+++ Set04/main.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	P1()
 	fmt.Println("Hi - set 04")
 }
---
+++ Set04/p1.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func P1() {
+	fmt.Println("run p1")
+}
