9bd69185a739dc33bfdf4f6d1e3aefcbded6ec6f
jaliyaL <jaliyalamahewa@gmail.com>
2025-09-10 23:47:53 +0530

    test

--- Set04/p2.go
+++ Set04/p2.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
 /*
@@ -10,57 +12,77 @@ Problem 2: Implement a simple pub-sub system using channels.
 */
 
 // buffered channel
-// func publish(i int, p chan int, wg *sync.WaitGroup) {
-// 	defer wg.Done()
-// 	p <- i
-
-// }
-
-// func P2() {
-// 	//fmt.Println("Hi p2")
-// 	pub := make(chan int, 10)
-// 	var wg sync.WaitGroup
-
-// 	for i := 1; i <= 10; i++ {
-// 		wg.Add(1)
-// 		go publish(i, pub, &wg)
-// 	}
-
-// 	wg.Wait()
-// 	close(pub)
-
-// 	for subscribe := range pub {
-// 		fmt.Println("values", subscribe)
-// 	}
-// }
-
-/////////// Mutex without buffer ////////////
-
-func publish(i int, pub chan int, wg *sync.WaitGroup, mu *sync.Mutex) {
+func publish(i int, p chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	mu.Lock()
-	pub <- i
-	mu.Unlock()
+	p <- i
 
 }
 
 func P2() {
-	pub := make(chan int)
+	//fmt.Println("Hi p2")
+	start := time.Now()
+	pub := make(chan int, 100000000)
 	var wg sync.WaitGroup
-	var mu sync.Mutex
 
-	for i := 1; i <= 10; i++ {
+	for i := 1; i <= 100000000; i++ {
 		wg.Add(1)
-		go publish(i, pub, &wg, &mu)
+		go publish(i, pub, &wg)
 	}
 
 	go func() {
-		for p := range pub {
-			fmt.Println(p)
-		}
+		wg.Wait()
+		close(pub)
 	}()
 
-	wg.Wait()
-	close(pub)
+	//var swg sync.WaitGroup
 
+	var counter int64 = 0
+	// swg.Add(1)
+	// go func(swg *sync.WaitGroup) {
+	// 	defer swg.Done()
+	// 	for range pub {
+	// 		//fmt.Println(subscribe)
+	// 		atomic.AddInt64(&counter, 1)
+	// 	}
+	// }(&swg)
+	// swg.Wait()
+
+	for range pub {
+		//fmt.Println(subscribe)
+		atomic.AddInt64(&counter, 1)
+	}
+
+	fmt.Println("elapsed time: ", time.Since(start))
+	fmt.Println("full records: ", counter)
 }
+
+/////////// Mutex without buffer ////////////
+
+// func publish(i int, pub chan int, wg *sync.WaitGroup, mu *sync.Mutex) {
+// 	defer wg.Done()
+// 	mu.Lock()
+// 	pub <- i
+// 	mu.Unlock()
+
+// }
+
+// func P2() {
+// 	pub := make(chan int)
+// 	var wg sync.WaitGroup
+// 	var mu sync.Mutex
+
+// 	for i := 1; i <= 10; i++ {
+// 		wg.Add(1)
+// 		go publish(i, pub, &wg, &mu)
+// 	}
+
+// 	go func() {
+// 		for p := range pub {
+// 			fmt.Println(p)
+// 		}
+// 	}()
+
+// 	wg.Wait()
+// 	close(pub)
+
+// }
