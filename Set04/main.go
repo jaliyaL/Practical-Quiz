852b8342b886322bdb161fdf5b727495c643e254
jaliyaL <jaliyalamahewa@gmail.com>
2025-09-10 17:23:50 +0530

    done

--- Set04/main.go
+++ Set04/main.go
@@ -1,10 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	P1()
-	fmt.Println("Hi - set 04")
+	//fmt.Println("Hi - set 04")
 }
--- Set04/p1.go
+++ Set04/p1.go
@@ -5,7 +5,29 @@ import "fmt"
 /*
 Problem 1: Write a function to remove duplicates from a slice of integers.
 */
+func removeDuplicates(s []int) []int {
+
+	check := make(map[int]bool)
+	cleanSlice := make([]int, 0)
+	duplicateSlice := make([]int, 0)
+
+	for _, val := range s {
+		if _, exists := check[val]; exists {
+			duplicateSlice = append(duplicateSlice, val)
+		} else {
+			check[val] = true
+			cleanSlice = append(cleanSlice, val)
+		}
+	}
+	fmt.Println(check)
+	fmt.Println("duplicate values", duplicateSlice)
+	return cleanSlice
+}
 
 func P1() {
-	fmt.Println("run p1")
+
+	oSlice := []int{1, 5, 6, 1, 4, 2, 8, 9, 2, 5}
+	nSlice := removeDuplicates(oSlice)
+	fmt.Println("original", oSlice)
+	fmt.Println("clean", nSlice)
 }
